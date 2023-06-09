60eea69b0c5a6c079b48531ec7c96f743547b85b
Raviprasaath Ramesh <117162868+Raviprasaath@users.noreply.github.com>
2023-06-09 22:24:23 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -9,3 +9,21 @@ const result = parser.xml2json(xml, options)
 const parsedResult = JSON.stringify(result, '');
 
 console.log(parsedResult);
+
+
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith('https');
+  }
+}
